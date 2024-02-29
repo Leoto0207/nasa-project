845aa360e896464ee8f04eb20900790c21b81c43
LeoTo <leoto0207@gmail.com>
2024-02-29 21:38:53 +0800

    load planets data in tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -2,11 +2,13 @@
 const request = require("supertest");
 const app = require("../../app");
 const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
+const { loadPlanetsData } = require("../../models/planets.model");
 
 describe("launches api", () => {
   //ensure mongo db set up before run the test
   beforeAll(async () => {
     await mongoConnect();
+    await loadPlanetsData();
   });
   //ensure disconnecting mongodb after the test
   afterAll(async () => {
